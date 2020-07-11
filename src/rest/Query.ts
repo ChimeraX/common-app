b9d657e42b22ea4abf84662a3beea3bd86f82348
SilviuMihnea <40487014+SilviuMihnea@users.noreply.github.com>
2020-07-11 11:11:08 +0300

    version 2

--- src/rest/Query.ts
+++ src/rest/Query.ts
@@ -1,9 +1,3 @@
 export default interface Query {
 	[key: string]: any;
 }
-
-export function createQuery(query: Query): string {
-	return Object.keys(query)
-		.map((key) => `${key}=${query[key]}`)
-		.join('&');
-}
